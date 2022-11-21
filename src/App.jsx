0df5375799fc8fe8a3a7b46c68a548544830773a
Bereke Sakan <bereke3103@mail.ru>
2022-11-21 08:33:40 +0600

    target-value

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { Component } from 'react';
 import './App.css';
 
+//!ФУНКЦИОНАЛЬНЫЙ КОМПОНЕНТ
+// function WhoAmI({ name, surname, link }) {
+//   // console.log(props);
+//   return (
+//     <div>
+//       <h1>
+//         My name is {name()} , surname - {surname}
+//       </h1>
+//       <a href={link} target="_blank">
+//         My profile
+//       </a>
+//     </div>
+//   );
+// }
+
+//!КЛАССОВЫЙ КОМПОНЕНТ
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props);
+    this.state = {
+      years: 27,
+      text: 'Увеличить год',
+      inputText: '',
+    };
+  }
+
+  onChangeInput = (e) => {
+    this.setState({
+      inputText: e.target.value,
+    });
+  };
+
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+  render() {
+    const { name, surname, link } = this.props;
+    const { years, inputText } = this.state;
+    return (
+      <div>
+        <h1>
+          My name is {name} , surname - {surname}
+        </h1>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h2>His years is {years}</h2>
+        <a href={link}>My Profile</a>
+
+        <div>My positin is - {inputText} </div>
+        <input type="text" onChange={this.onChangeInput} />
+      </div>
+    );
+  }
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <WhoAmI name="Bereke" surname="Sakan" link="https://www.youtube.com/" />
+      <WhoAmI name="Alex" surname="Smith" link="https://ru-ru.facebook.com/" />
+    </div>
+  );
 }
 
 export default App;
